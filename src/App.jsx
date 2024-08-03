3bf1272716922c1e266de918a10a372e638ad1f0
iti-0204 <yasuken0327@icloud.com>
2024-08-03 17:28:27 +0900

    [Add] react-icon導入

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Button from "./components/Button";
-import Header from "./components/Header";
+// import Header from "./components/Header";
 
 function App() {
   return (
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { BiChevronRight } from "react-icons/bi";
 
 const Button = () => {
   return (
     <button className="bg-primary w-48 h-12 text-white text-xl font-en tracking-en rounded">
       Works
+      <BiChevronRight className="inline-block ml-6 text-2xl font-bold align-text-top" />
     </button>
   );
 };
