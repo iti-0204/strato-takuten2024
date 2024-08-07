a6bc8bc39d22015f05c7a0b96a2e22648f1c9805
iti-0204 <yasuken0327@icloud.com>
2024-08-07 18:24:59 +0900

    [Add] FVレイアウト実装

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,30 @@ import Flow from "./components/Flow";
 // import Title from "./components/Title";
 // import Header from "./components/Header";
 
+import { BiChevronDown } from "react-icons/bi";
+
 function App() {
   return (
     <div>
-      {/* <div className="bg-fv w-full h-screen bg-cover">
-        <h1>Strato</h1>
-      </div> */}
+      <div id="mainvisual">
+        <div className="bg-fv w-full h-screen bg-cover text-center pt-[20%]">
+          <img
+            src="/images/logo.png"
+            alt="Stratoロゴ"
+            className="mx-auto mb-6"
+          />
+          <p className="font-en tracking-en text-base text-primary mb-9">
+            UI/UX <br />
+            Service Design
+          </p>
+          <Button type={"btnL"} />
+          <p className="font-en tracking-en text-xl text-primary mt-20 mb-2">
+            scroll
+          </p>
+          <BiChevronDown color={"#FC840D"} size={24} className="mx-auto" />
+        </div>
+      </div>
       {/* <Header /> */}
-      <Button type={"UI"} />
 
       {/* <Title type={"center"}/> */}
       {/* <Card
