9d6587630c9d3aec46d3a7a30800352ffa2f05de
iti-0204 <yasuken0327@icloud.com>
2024-08-07 15:42:50 +0900

    [Add] Flowコンポーネントのグラデーション2実装

--- src/components/Flow.jsx
+++ src/components/Flow.jsx
@@ -2,9 +2,18 @@ import React from "react";
 
 const Flow = () => {
   return (
-    <h2 className="text-transparent text-base font-en tracking-en bg-gradient-to-b inline-block bg-clip-text relative step-gradation">
-      Step
-    </h2>
+    <div>
+      <div>
+        <p className="text-transparent text-base font-en tracking-en bg-gradient-to-b inline-block bg-clip-text relative step-gradation1">
+          Step
+        </p>
+      </div>
+      <div>
+        <p className="text-transparent text-3.5xl font-en tracking-en bg-gradient-to-b inline-block bg-clip-text relative step-gradation2">
+          01
+        </p>
+      </div>
+    </div>
   );
 };
 
