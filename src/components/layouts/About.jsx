dde9ca90b9da0014f8ff5ff1e49ae0b4fe497fc9
iti-0204 <yasuken0327@icloud.com>
2024-08-08 19:11:48 +0900

    [Add] Aboutコンポーネントレイアウト実装

--- src/components/layouts/About.jsx
+++ src/components/layouts/About.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import Title from "../ui/Title";
 
 const About = () => {
   return (
-    <div className="bg-fv w-full h-screen bg-cover text-center">
-
+    <div className="bg-fv w-full h-[90vh] bg-cover text-center font-jp tracking-jp text-black">
+      <div className="pt-[88px]">
+        <Title type={"center"} />
+        <p className="font-bold text-[32px] mt-20 mb-6">
+          プロセスを層にしてサービスを作る卓
+        </p>
+        <p>
+          様々な段階を踏まえながら、みんなのアイデアを出し合い
+          <br />
+          UI/UXデザインプロセスを楽しみながらサービスを考える卓です。
+          <br />
+          最終的な成果物はアプリケーションデザイン/webデザインとなります。
+        </p>
+      </div>
     </div>
-  )
+  );
 };
 
 export default About;
