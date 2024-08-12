05f3ac7d4b6dbf63e5062f3570979f3a1235ed27
iti-0204 <yasuken0327@icloud.com>
2024-08-12 15:22:10 +0900

    [Update] Footerコンポーネントのレスポンシブ

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -3,20 +3,22 @@ import { BiChevronUp } from "react-icons/bi";
 
 const Footer = () => {
   return (
-    <div className="mx-auto flex justify-between h-[35vh] items-center max-w-[1000px] font-en tracking-en font-normal">
+    <div className="tablet:mx-auto ml-[42px] flex tablet:flex-row flex-col tablet:justify-between justify-center h-[35vh] items-center tablet:max-w-[1000px] font-en tracking-en font-normal max-w-[50%]">
       <div></div>
       <div className="text-center">
-        <div className="flex gap-6 mb-6">
-          <img src="/images/logo.png" alt="" />
+        <div className=" tablet:flex gap-6 mb-6">
+          <img src="/images/logo.png" alt="" className="mb-6 tablet:mb-0" />
           <p className="text-left">
             UI/UX <br />
             Service Design
           </p>
         </div>
-        <p className="text-[10px]">© Strato All Rights Reserved.</p>
+        <p className="text-[10px] text-left tablet:text-center">
+          © Strato All Rights Reserved.
+        </p>
       </div>
-      <div className="text-primary">
-        <BiChevronUp color={"#FC840D"} className="mx-auto" size={24}/>
+      <div className="text-primary tablet:block hidden">
+        <BiChevronUp color={"#FC840D"} className="mx-auto" size={24} />
         <p className="mt-[9px] font-bold">TOP</p>
       </div>
     </div>
