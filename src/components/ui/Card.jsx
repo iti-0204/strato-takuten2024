5ed301771b8b2540e702dabd85b735d154f39888
iti-0204 <yasuken0327@icloud.com>
2024-08-13 19:35:31 +0900

    [Update] Cardコンポーネントの画像追加、Props追加

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 
-const Card = ({ team, title, caption }) => {
+const Card = ({ team, title, caption, img }) => {
   return (
     <div className="tablet:w-card tablet:h-card w-[335px] h-auto p-6 shadow-card rounded-2xl text-left">
-      <img
-        src="/images/background.png"
-        alt=""
-        className="w-full h-cardImg object-cover mb-4"
-      />
+      <img src={img} alt="" className="w-full h-cardImg object-cover mb-4" />
       <p className="text-gray text-xl font-jp mb-1">{team}</p>
       <p className="text-primary text-3.5xl font-jp font-black mb-4">{title}</p>
       <p className="text-black text-base font-jp">{caption}</p>
