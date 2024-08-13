1e6abb98f7321b3d340ce4b885961d9cccc2dc29
iti-0204 <yasuken0327@icloud.com>
2024-08-13 22:04:03 +0900

    [Add] Exhibitコンポーネントのレイアウト

--- src/components/layouts/Exhibit.jsx
+++ src/components/layouts/Exhibit.jsx
@@ -1,10 +1,37 @@
 import React from "react";
 import Header from "./Header";
+import Button from "../ui/Button";
 
 const Exhibit = () => {
   return (
     <div>
       <Header></Header>
+      <div className="w-[85%] flex justify-between mx-auto font-jp tracking-jp font-bold">
+        <img
+          src="/images/panel/chotohayaPanel.png"
+          alt=""
+          className="w-[64%]"
+        />
+        <div className="shadow-card px-[50px] w-[35%] h-fit">
+          <div className="text-center">
+            <img
+              src="/images/members/tyotohaya.png"
+              alt=""
+              className="w-[240px] mx-auto"
+            />
+            <p>ちょっと早朝やなのに。</p>
+          </div>
+          <div className="flex justify-between">
+            <Button type={"btnM"} />
+            <Button type={"btnM"} />
+          </div>
+          <div className="flex flex-wrap justify-between">
+            <img src="/images/members/misen.png" alt="" className="w-40" />
+            <img src="/images/members/you.png" alt="" className="w-40" />
+            <img src="/images/members/chihiro.png" alt="" className="w-40" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
