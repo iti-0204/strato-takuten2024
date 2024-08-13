481d79a5386c18da891ff59b2de87c63c2d16879
iti-0204 <yasuken0327@icloud.com>
2024-08-13 18:27:50 +0900

    [Add] React router設定追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,28 @@
 import "./App.css";
-import Header from "./components/layouts/Header";
-import Fv from "./components/layouts/Fv";
-import About from "./components/layouts/About";
-import Works from "./components/layouts/Works";
-import Flow from "./components/layouts/Flow";
-import Members from "./components/layouts/Members";
+// import Header from "./components/layouts/Header";
+// import Fv from "./components/layouts/Fv";
+// import About from "./components/layouts/About";
+// import Works from "./components/layouts/Works";
+// import Flow from "./components/layouts/Flow";
+// import Members from "./components/layouts/Members";
 
-import PcAnimation from "./PcAnimation.json";
-import SpAnimation from "./SpAnimation.json";
-import TabletAnimation from "./TabletAnimation.json";
+// import PcAnimation from "./PcAnimation.json";
+// import SpAnimation from "./SpAnimation.json";
+// import TabletAnimation from "./TabletAnimation.json";
 
-import { Player } from "@lottiefiles/react-lottie-player";
-import { useMediaQuery } from "react-responsive";
-import Footer from "./components/layouts/Footer";
+// import { Player } from "@lottiefiles/react-lottie-player";
+// import { useMediaQuery } from "react-responsive";
+// import Footer from "./components/layouts/Footer";
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import Home from "./components/layouts/Home";
 
 function App() {
-  // メディアクエリ
-  const isTablet = useMediaQuery({ query: "(min-width: 768px)" });
-  const isPC = useMediaQuery({ query: "(min-width: 1024px)" });
-
-  // Lottie振り分け
-  let lottieSrc = SpAnimation;
-  if (isPC) {
-    lottieSrc = PcAnimation;
-  } else if (isTablet) {
-    lottieSrc = TabletAnimation;
-  }
-
-  // LottieComplete時の処理
-  const doSomething = () => {
-    var firstView = document.getElementById("first-view");
-    var fadeIn = document.getElementById("mainvisual");
-    firstView.classList.add("delete");
-    console.log(fadeIn);
-    fadeIn.classList.add("fadeIn");
-  };
-
   return (
-    <div>
-      <div id="mainvisual">
-        <Player
-          autoplay
-          loop={false}
-          src={lottieSrc}
-          onEvent={(event) => {
-            console.log(event);
-            if (event === "complete") {
-              doSomething();
-              console.log(lottieSrc);
-            }
-          }}
-          id="first-view"
-          className="h-screen scale-[1.3]"
-        />
-
-        <Fv />
-      </div>
-
-      <Header />
-
-      <section id="about">
-        <About />
-      </section>
-
-      <section id="works">
-        <Works />
-      </section>
-
-      <section id="flow" className="bg-fv w-full bg-cover">
-        <Flow />
-      </section>
-
-      <section id="members">
-        <Members />
-      </section>
-
-      <footer className="bg-[#FFF1E4]">
-        <Footer />
-      </footer>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/layouts/Home.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+
+import Header from "./Header";
+import Fv from "./Fv";
+import About from "./About";
+import Works from "./Works";
+import Flow from "./Flow";
+import Members from "./Members";
+import Footer from "./Footer";
+
+import PcAnimation from "../../PcAnimation.json";
+import SpAnimation from "../../SpAnimation.json";
+import TabletAnimation from "../../TabletAnimation.json";
+
+import { Player } from "@lottiefiles/react-lottie-player";
+import { useMediaQuery } from "react-responsive";
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+
+const Home = () => {
+  // メディアクエリ
+  const isTablet = useMediaQuery({ query: "(min-width: 768px)" });
+  const isPC = useMediaQuery({ query: "(min-width: 1024px)" });
+
+  // Lottie振り分け
+  let lottieSrc = SpAnimation;
+  if (isPC) {
+    lottieSrc = PcAnimation;
+  } else if (isTablet) {
+    lottieSrc = TabletAnimation;
+  }
+
+  // LottieComplete時の処理
+  const doSomething = () => {
+    var firstView = document.getElementById("first-view");
+    var fadeIn = document.getElementById("mainvisual");
+    firstView.classList.add("delete");
+    console.log(fadeIn);
+    fadeIn.classList.add("fadeIn");
+  };
+
+  return (
+    <div>
+      <div id="mainvisual">
+        <Player
+          autoplay
+          loop={false}
+          src={lottieSrc}
+          onEvent={(event) => {
+            console.log(event);
+            if (event === "complete") {
+              doSomething();
+              console.log(lottieSrc);
+            }
+          }}
+          id="first-view"
+          className="h-screen scale-[1.3]"
+        />
+
+        <Fv />
+      </div>
+
+      <Header />
+
+      <section id="about">
+        <About />
+      </section>
+
+      <section id="works">
+        <Works />
+      </section>
+
+      <section id="flow" className="bg-fv w-full bg-cover">
+        <Flow />
+      </section>
+
+      <section id="members">
+        <Members />
+      </section>
+
+      <footer className="bg-[#FFF1E4]">
+        <Footer />
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
