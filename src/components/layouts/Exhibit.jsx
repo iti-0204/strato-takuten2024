cdc90c47d499e90b7696b877d7fd37278225ab11
iti-0204 <yasuken0327@icloud.com>
2024-08-14 16:08:58 +0900

    [Add] Exhibitコンポーネントの下部レイアウト

--- src/components/layouts/Exhibit.jsx
+++ src/components/layouts/Exhibit.jsx
@@ -6,29 +6,38 @@ const Exhibit = () => {
   return (
     <div>
       <Header></Header>
-      <div className="w-[85%] flex justify-between mx-auto font-jp tracking-jp font-bold">
+      <div className="w-[85%] flex justify-between mx-auto font-jp tracking-jp font-bold ">
         <img
           src="/images/panel/chotohayaPanel.png"
           alt=""
           className="w-[64%]"
         />
-        <div className="shadow-card px-[50px] w-[35%] h-fit">
+        <div className="shadow-card p-[3%] w-[35%] h-fit">
           <div className="text-center">
             <img
               src="/images/members/tyotohaya.png"
               alt=""
-              className="w-[240px] mx-auto"
+              className="w-[55%] mx-auto "
             />
-            <p>ちょっと早朝やなのに。</p>
+            <p className="mt-4 mb-[36px]">ちょっと早朝やなのに。</p>
           </div>
           <div className="flex justify-between">
             <Button type={"btnM"} title={"Works"} />
             <Button type={"btnM"} />
           </div>
-          <div className="flex flex-wrap justify-between">
-            <img src="/images/members/misen.png" alt="" className="w-40" />
-            <img src="/images/members/you.png" alt="" className="w-40" />
-            <img src="/images/members/chihiro.png" alt="" className="w-40" />
+          <div className="flex flex-wrap justify-between text-sm mt-[36px]">
+            <div className="w-[45%] text-center">
+              <img src="/images/members/misen.png" alt="" className="mb-4" />
+              <p className="inline text-center">3M みせん</p>
+            </div>
+            <div className="w-[45%] text-center">
+              <img src="/images/members/you.png" alt="" className="mb-4" />
+              <p className="inline text-center">3M よう</p>
+            </div>
+            <div className="w-[45%] text-center mx-auto">
+              <img src="/images/members/chihiro.png" alt="" className="mb-4" />
+              <p className="inline text-center">3M ちひろ</p>
+            </div>
           </div>
         </div>
       </div>
